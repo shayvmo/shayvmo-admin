50d3a2888828ca9707a34d1385eaa6d4922647a5
liuhaibin <1006897172@qq.com>
2022-01-28 11:13:46 +0800

    [*] 优化

--- config/staticfile.php
+++ config/staticfile.php
@@ -3,10 +3,18 @@
 
 
 // 后台静态文件存放目录
-const BE_ADMIN = '/static/admin/admin/';
-const BE_CONFIG = '/static/admin/config/';
-const BE_COMPONENT = '/static/admin/component/';
+if (!defined('BE_ADMIN')) {
+    define("BE_ADMIN", '/static/admin/admin/');
+}
+if (!defined('BE_CONFIG')) {
+    define("BE_CONFIG", '/static/admin/config/');
+}
+if (!defined('BE_COMPONENT')) {
+    define("BE_COMPONENT", '/static/admin/component/');
+}
 
 // 文件上传目录
-const UPLOADS = '/uploads/';
+if (!defined('UPLOADS')) {
+    define("UPLOADS", '/uploads/');
+}
 
